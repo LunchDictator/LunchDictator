992b33be2b66c8a304f955db28d3c91ebcd4bbb7
Pawel Burzynski <p.k.burzynski@gmail.com>
2014-09-04 23:02:03 +0100

    database link

--- src/LunchDictator/Controllers/HomeController.cs
+++ src/LunchDictator/Controllers/HomeController.cs
@@ -1,53 +1,26 @@
 ﻿namespace LunchDictator.Controllers
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Mvc;
 
+    using LunchDictator.DataAccess;
     using LunchDictator.Models;
 
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            var model = new HomeIndexViewModel
+            using (var ctx = new LunchContext())
             {
-                Places =
-                    new List<PlaceViewModel>
-                    {
-                        new PlaceViewModel
-                        {
-                            ImageUrl = "http://www.dalton-park.co.uk/wp-content/uploads/2009/06/subway_705.jpg",
-                            Name = "Subway"
-                        },
-                        new PlaceViewModel
-                        {
-                            ImageUrl = "http://intu.co.uk/uploads/tortilla-logo-v2.jpg",
-                            Name = "Tortilla"
-                        },
-                        new PlaceViewModel
-                        {
-                            ImageUrl = "http://www.fastfind.info/photos/greggs_phppogjnl_big.jpg",
-                            Name = "Greggs"
-                        },
-                        new PlaceViewModel
-                        {
-                            ImageUrl = "http://www.reed.co.uk/resources/cms/images/logos/Banner_17236.png",
-                            Name = "Wenzel's"
-                        },
-                        new PlaceViewModel
-                        {
-                            ImageUrl = "http://www.londondiscountvouchers.com/wp-content/themes/couponpress/thumbs/BananaTree_logo.jpg",
-                            Name = "Banana Tree"
-                        },
-                        new PlaceViewModel
-                        {
-                            ImageUrl = "http://www.watford.gov.uk/ccm/cms-service/stream/image/?image_id=18880016",
-                            Name = "The Market"
-                        }
-                    }
-            };
+                var model = new HomeIndexViewModel
+                {
+                    Places =
+                        ctx.Places.Select(p => new PlaceViewModel { ImageUrl = p.ImageUrl, Name = p.Name }).ToList()
+                };
 
-            return this.View(model);
+                return this.View(model);
+            }
         }
     }
 }
--- src/LunchDictator/DataAccess/Migrations/Configuration.cs
+++ src/LunchDictator/DataAccess/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ namespace LunchDictator.DataAccess.Migrations
     using System.Data.Entity.Migrations;
 
     using LunchDictator.DataAccess;
+    using LunchDictator.DataAccess.Entities;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LunchContext>
     {
@@ -12,7 +13,41 @@ namespace LunchDictator.DataAccess.Migrations
         }
 
         protected override void Seed(LunchContext context)
-        {            
+        {
+            context.Places.AddOrUpdate(
+                e => e.Id,
+                new Place
+                {
+                    Id = 1,
+                    ImageUrl = "http://www.dalton-park.co.uk/wp-content/uploads/2009/06/subway_705.jpg",
+                    Name = "Subway"
+                },
+                new Place { Id = 1, ImageUrl = "http://intu.co.uk/uploads/tortilla-logo-v2.jpg", Name = "Tortilla" },
+                new Place
+                {
+                    Id = 2,
+                    ImageUrl = "http://www.fastfind.info/photos/greggs_phppogjnl_big.jpg",
+                    Name = "Greggs"
+                },
+                new Place
+                {
+                    Id = 3,
+                    ImageUrl = "http://www.reed.co.uk/resources/cms/images/logos/Banner_17236.png",
+                    Name = "Wenzel's"
+                },
+                new Place
+                {
+                    Id = 4,
+                    ImageUrl =
+                        "http://www.londondiscountvouchers.com/wp-content/themes/couponpress/thumbs/BananaTree_logo.jpg",
+                    Name = "Banana Tree"
+                },
+                new Place
+                {
+                    Id = 5,
+                    ImageUrl = "http://www.watford.gov.uk/ccm/cms-service/stream/image/?image_id=18880016",
+                    Name = "The Market"
+                });
         }
     }
 }
