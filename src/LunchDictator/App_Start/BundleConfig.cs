ed7dbfdccdf6cd26d4b0295329ac6e9e8c4dc250
Pawel Burzynski <pawelburzynski@zagzig.co.uk>
2014-09-04 15:44:48 +0100

    Initail design

--- src/LunchDictator/App_Start/BundleConfig.cs
+++ src/LunchDictator/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace LunchDictator
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                     // "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
