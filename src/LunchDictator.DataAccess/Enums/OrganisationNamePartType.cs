6ff1ed38516d662b4dfd19cbff330fc0d616be8e
Richard Moran <richardmoran@moneydebtandcredit.com>
2014-09-20 21:24:41 +0100

    Add organisation name part entities

---
+++ src/LunchDictator.DataAccess/Enums/OrganisationNamePartType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchDictator.DataAccess.Enums
+{
+    public enum OrganisationNamePartType
+    {
+        Adverb = 0,
+        Verb = 1,
+        Adjective = 2,
+        Noun = 3
+    }
+}
